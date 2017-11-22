79f28d3bc98d9559186d7b4f837c30f722ae0d66
Andres Rubio <andresfra92@gmail.com>
2017-11-22 19:51:12 -0400

    Probando ws

--- viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/domainLogicLayer/M03_AdministracionVideos/Video.java
+++ viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/domainLogicLayer/M03_AdministracionVideos/Video.java
@@ -2,5 +2,10 @@ package edu.ucab.desarrollo.viucab.domainLogicLayer.M03_AdministracionVideos;
 
 public class Video {
 
+    private int _id;
+    private String _titulo;
+    private String _descripcion;
+
+
 
 }
