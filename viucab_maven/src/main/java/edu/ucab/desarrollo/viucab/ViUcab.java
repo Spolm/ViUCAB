750e9a1a1db267a69cb8b166adaf741e55c1f0d1
Unknown <aagilazer_95@hotmail.com>
2017-11-25 17:43:44 -0400

    quite webxml config por la actual de develop

--- viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/ViUcab.java
+++ viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/ViUcab.java
@@ -1,6 +1,8 @@
 package edu.ucab.desarrollo.viucab;
 
 import edu.ucab.desarrollo.viucab.webService.*;
+import edu.ucab.desarrollo.viucab.webService.M07_Etiqueta.M07_Etiqueta;
+import edu.ucab.desarrollo.viucab.webService.M11_Estadistica.M11_Estadistica;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -14,6 +16,8 @@ public class ViUcab extends Application {
     public Set<Class<?>> getClasses() {
         HashSet h= new HashSet<Class<?>>();
         h.add(hola.class);
+        h.add(M11_Estadistica.class);
+        h.add(M07_Etiqueta.class);
         return h;
     }
 }
