8b7a321bc1474430bc2ec5f78eda33ab01ffba6e
mariaEPerezF <esteperez1593@gmail.com>
2017-12-04 14:08:25 -0400

    Agregando Barra de Busqueda

--- IonicProject/src/pages/Modulo 2/inicio/inicio.ts
+++ IonicProject/src/pages/Modulo 2/inicio/inicio.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 
 /**
@@ -15,14 +15,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'inicio.html',
 })
 export class InicioPage {
+  
+  buscarQuery : string ='';
+  
+  items: any[];
+  color: 'light';
+  estadoNombre:string;
 
   @ViewChild('myNav') nav: NavController
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InicioPage');
   }
 
+  initializeItems(){
+    {
+      this.items =[{
+      nombre: 'Marty',
+      suscritos: '133',
+      color: this.color,
+      estado: 'Suscrito',
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeK0JpMddFw6P1hVBdjiQQuw7MpFnGza014TUmxfGfy4B9uYpa'},
+      {
+        nombre: 'Martin',
+        suscritos: '133',
+        color: this.color,
+        estado: 'Suscrito',
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeK0JpMddFw6P1hVBdjiQQuw7MpFnGza014TUmxfGfy4B9uYpa'}]
+     }
+  }
 
+  getItems(ev: any){
+    //Reset items back to all of the items
+    this.initializeItems();
+    
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+    
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+          this.items = this.items.filter((item) => {
+            return (item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+        }
+  }
 }
--- IonicProject/src/pages/Modulo 2/suscripciones/suscripciones.ts
+++ IonicProject/src/pages/Modulo 2/suscripciones/suscripciones.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController,AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the SuscripcionesPage page.
@@ -14,13 +14,51 @@ import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-an
   templateUrl: 'suscripciones.html',
 })
 export class SuscripcionesPage {
+  buscarQuery : string ='';
+  
+  items: any[];
+  color: 'light';
+  estadoNombre:string;
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController) {
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController,public alertCtrl: AlertController) {
+    this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SuscripcionesPage');
   }
+  initializeItems(){
+    {
+      this.items =[{
+      nombre: 'Marty',
+      suscritos: '133',
+      color: this.color,
+      estado: 'Suscrito',
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeK0JpMddFw6P1hVBdjiQQuw7MpFnGza014TUmxfGfy4B9uYpa'},
+      {
+        nombre: 'Martin',
+        suscritos: '133',
+        color: this.color,
+        estado: 'Suscrito',
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeK0JpMddFw6P1hVBdjiQQuw7MpFnGza014TUmxfGfy4B9uYpa'}]
+     }
+  }
 
+  getItems(ev: any){
+    //Reset items back to all of the items
+    this.initializeItems();
+    
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+    
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+          this.items = this.items.filter((item) => {
+            return (item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+        }
+  }
 
 }
