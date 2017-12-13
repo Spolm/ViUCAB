94abfe17560bd631d38352d6cf2c63fab0d30e3f
bgfa26 <bgfa26@gmail.com>
2017-12-12 20:10:56 -0400

    Arreglos en detalles de algunos archivos

--- IonicProject/src/pages/Modulo 2/inicio/inicio.ts
+++ IonicProject/src/pages/Modulo 2/inicio/inicio.ts
@@ -18,10 +18,39 @@ import { Subscription } from 'rxjs/Subscription';
   providers: [RestApiService]
 })
 export class InicioPage {
+  /*data = [{                       //DATA DE PRUEBA LOCAL
+      id: 1,
+      title: 'Video 1',
+      user: 'Barbara Fernández',
+      date: 'November 5, 1955'
+  }, {
+      id: 2,
+      title: 'Video 2',
+      user: 'Barbara Fernández',
+      date: 'November 6, 1955'
+  }, {
+      id: 3,
+      title: 'Video 3',
+      user: 'Barbara Fernández',
+      date: 'November 7, 1955'
+  }, {
+      id: 4,
+      title: 'Video 4',
+      user: 'Barbara Fernández',
+      date: 'November 8, 1955'
+  }, {
+      id: 5,
+      title: 'Video 5',
+      user: 'Barbara Fernández',
+      date: 'November 9, 1955'
+  }, {
+      id: 6,
+      title: 'Video 6',
+      user: 'Barbara Fernández',
+      date: 'November 5, 1955'
+  }];*/
   data: any[];
-  
   buscarQuery : string ='';
-  
   items: any[];
   color: 'light';
   estadoNombre:string;
