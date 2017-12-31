ba7334ea7014079d4eeeb6c9ba8f63d64b7d84d6
carlosmachinp <carlosmachinp@gmail.com>
2017-12-30 20:29:16 -0400

    modulo 5 ws funcionando con back

--- viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/webService/M05_ListaDeReproduccion/M05_ListaDeReproduccion.java
+++ viucab_maven/src/main/java/edu/ucab/desarrollo/viucab/webService/M05_ListaDeReproduccion/M05_ListaDeReproduccion.java
@@ -4,20 +4,28 @@ import com.google.gson.Gson;
 import edu.ucab.desarrollo.viucab.common.entities.Entity;
 import edu.ucab.desarrollo.viucab.common.entities.EntityFactory;
 import edu.ucab.desarrollo.viucab.common.entities.ListaDeReproduccion;
+import edu.ucab.desarrollo.viucab.common.exceptions.M08.BdConnectException;
+import edu.ucab.desarrollo.viucab.common.exceptions.M08.PlConnectException;
+
 import edu.ucab.desarrollo.viucab.domainLogicLayer.Command;
 import edu.ucab.desarrollo.viucab.domainLogicLayer.CommandsFactory;
 import edu.ucab.desarrollo.viucab.domainLogicLayer.M05_ListaDeReproduccion.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import java.util.ArrayList;
 
 @Path("/playlist")
 public class M05_ListaDeReproduccion {
 
     Gson gson = new Gson();
-
+    private static Logger logger = LoggerFactory.getLogger( M05_ListaDeReproduccion.class );
+/*
     @GET
     @Path("/editPlaylist")
     @Produces("text/plain")
@@ -27,17 +35,55 @@ public class M05_ListaDeReproduccion {
             Entity listaObject = EntityFactory.listaDeReproduccion(1); //SE LE ESTA PASANDO MANUALMENTE
             Command commandGetLista = CommandsFactory.instanciaGetLista(listaObject);
             GetListaComando cmd = (GetListaComando) commandGetLista;
-            return (ListaDeReproduccion) cmd.execute();
+            //return (ListaDeReproduccion) cmd.execute(); NUEVO COMENTADOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO solo esta linea
             /*Entity result = cmd.Return();
             ListaDeReproduccion json = (ListaDeReproduccion) result;*/
 
-
-        } catch (ViUCABException e) {
+/*
+        } catch (Exception e) {//ViUCABException e) {
             //TODO: handle exception
-           throw new WebFault(e.Mensaje, e.codigo);
+           //throw new WebFault(e.Mensaje, e.codigo);
         }
 
        
+    }*/
+
+    @GET
+    @Path("/editPlaylist")
+    @Produces("text/plain")
+    public String obtenerLista(@QueryParam("id_usu") int id_usu)
+    {
+        Entity listaObject = EntityFactory.obtenerLista(1, id_usu);
+        Command commandGetLista = CommandsFactory.instanciaGetLista(listaObject);
+        GetListaComando cmd = (GetListaComando) commandGetLista;
+
+        try {
+            cmd.execute();
+            Entity result = cmd.Return();
+            ListaDeReproduccion json = (ListaDeReproduccion) result;
+            ArrayList<Entity> resultados =cmd.getResultados();
+            return gson.toJson(resultados);
+/*
+        } catch (VIUCABException e) {
+
+            listaObject.set_errorCode(e.ERROR_CODE);
+            listaObject.set_errorMsg(e.ERROR_MSG);
+            logger.error("Metodo: {} {}", "getTrainingDetail", e.toString());
+            return gson.toJson(listaObject);*/
+        } catch (PlConnectException e) {
+            e.printStackTrace();
+            listaObject.set_errorCode(550);
+            listaObject.set_errorMsg(e.ERROR_MSG);
+            logger.error("Metodo: {} {}", "getTrainingDetail", e.toString());
+            return gson.toJson(listaObject);
+        } catch (BdConnectException e) {
+            e.printStackTrace();
+            listaObject.set_errorCode(550);
+            listaObject.set_errorMsg(e.ERROR_MSG);
+            logger.error("Metodo: {} {}", "getTrainingDetail", e.toString());
+            return gson.toJson(listaObject);
+        }
+
     }
 
     @GET
@@ -45,7 +91,7 @@ public class M05_ListaDeReproduccion {
     @Produces("text/plain")
     public String ejemplo()
     {
-        return "Soy una putica loca";
+        return "Soy una putica loca loca de verdad";
 
        
     }
