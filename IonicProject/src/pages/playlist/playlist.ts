6b359b830dfbac48283d3c67369ea1b0471ed0ef
carlosmachinp <carlosmachinp@gmail.com>
2018-01-09 18:27:35 -0400

    vainas de front

--- IonicProject/src/pages/playlist/playlist.ts
+++ IonicProject/src/pages/playlist/playlist.ts
@@ -1,8 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams ,AlertController} from 'ionic-angular';
 import {EditListPage} from "./edit-list/edit-list";
 import { PopoverController } from 'ionic-angular';
 import {ViewListPage} from "./view-list/view-list";
+import { RestApiService } from "../../app/rest-api.service";
+import { Content } from 'ionic-angular';
+import { NullAstVisitor } from '@angular/compiler';
 
 import {PopOverListasReproduccionPage} from '../playlist/pop-over-listas-reproduccion/pop-over-listas-reproduccion';
 import {AddListPage} from "./add-list/add-list";
@@ -18,10 +21,12 @@ import {AddListPage} from "./add-list/add-list";
 @Component({
   selector: 'page-playlist',
   templateUrl: 'playlist.html',
+  providers: [RestApiService]
 })
 export class PlaylistPage {
 
   public tab: string;
+  public respu:any;
   public ListasDeReproduccion = [
     { title: 'Lista numero 1', amount: '13', duration: '30:00 min', img:'https://www.lapatilla.com/site/wp-content/uploads/2017/08/Meme.x43795.jpg'},
     { title: 'Lista numero 2', amount: '3', duration: '3:00 min' , img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtXnCR_ZKLXop4kwMfaIDLOxcPqco1zROOTIoEyCSP5LtBuXho'},
@@ -29,14 +34,41 @@ export class PlaylistPage {
   ];
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController) {
+  constructor(public api: RestApiService, public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController,public alertCtrl: AlertController) {
   }
+
+  
 
   @ViewChild('myNav') nav: NavController
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListasPage');
     this.tab = 'mias';
+    console.log("ENtre a playlist");
+    this.getPlaylists();
   }
+
+  getPlaylists(){
+
+    this.api.getPlaylists('playlist/getAllPlaylist',4).subscribe((data) => { // Success
+
+      console.log(data);
+     },
+     (error) =>{
+       console.error(error);
+     });
+
+    // this.api.getTodoParam('playlist/getAllPlaylist',4);
+    /*
+    .map((data) => {
+      console.log (data);
+     },
+     (error) =>{
+       console.error(error);
+     });
+     */
+  }
+
+  
 
   presentPopover(myEvent) {
     let popover = this.popoverCtrl.create(PopOverListasReproduccionPage);
